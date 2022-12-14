387f73f48f038d7c55343390f3de70916c202e58
SehyeonKang <gedfgrdt@naver.com>
2022-12-14 12:47:29 +0900

    프로젝트 생성, DB 연동

---
+++ src/main/java/com/snowflower/communitypractice/CommunityPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.snowflower.communitypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowflower/communitypractice/CommunityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowflower.communitypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
