e10e35c3904f07dc8fee3a2f2ad5c00b248213cd
SehyeonKang <gedfgrdt@naver.com>
2022-12-16 16:10:35 +0900

    Feat: 게시글 상세 페이지 추가
    게시글 상세 페이지 추가, 게시글 수정과 삭제, 조회수 증가 기능 추가

--- src/main/java/com/snowflower/communitypractice/repository/BoardRepository.java
+++ src/main/java/com/snowflower/communitypractice/repository/BoardRepository.java
@@ -1,7 +1,15 @@
 package com.snowflower.communitypractice.repository;
 
 import com.snowflower.communitypractice.domain.board.Board;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    /**
+     * 게시글 리스트 조회 - (삭제 여부 기준)
+     */
+    List<Board> findAllByDeleteYn(final char deleteYn, final Sort sort);
 }
--- src/main/java/com/snowflower/communitypractice/service/BoardService.java
+++ src/main/java/com/snowflower/communitypractice/service/BoardService.java
@@ -41,6 +41,27 @@ public class BoardService {
     }
 
     /**
+     * 게시글 리스트 조회 - (삭제 여부 기준)
+     */
+    public List<BoardResponseDto> findAllByDeleteYn(final char deleteYn) {
+
+        Sort sort = Sort.by(Sort.Direction.DESC, "id", "createdDate");
+        List<Board> list = boardRepository.findAllByDeleteYn(deleteYn, sort);
+        return list.stream().map(BoardResponseDto::new).collect(Collectors.toList());
+    }
+
+    /**
+     * 게시글 상세정보 조회
+     */
+    @Transactional
+    public BoardResponseDto findById(final Long id) {
+
+        Board entity = boardRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
+        entity.increaseHits();
+        return new BoardResponseDto(entity);
+    }
+
+    /**
      * 게시글 수정
      */
     @Transactional
@@ -48,6 +69,17 @@ public class BoardService {
 
         Board entity = boardRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
         entity.update(params.getTitle(), params.getContent(), params.getWriter());
+        return id;
+    }
+
+    /**
+     * 게시글 삭제
+     */
+    @Transactional
+    public Long delete(final Long id) {
+
+        Board entity = boardRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.POSTS_NOT_FOUND));
+        entity.delete();
         return id;
     }
 }
